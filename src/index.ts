3a40ed3f2cf6496505a4304c13a69e08576f4f1a
Rafael Ferreira de Souza <rafael.souza@sisplansistemas.com.br>
2021-11-24 13:19:26 -0300

    chore: added logger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import { logger } from './common/logger'
 import { server } from './server'
 
 /**
  * starting server
  */
 
-server().then(app => app.listen(3000, () => console.log('Server is running on port 3000')))
+server().then(app => app.listen(3000, () => logger.info('Server is running on port 3000')))
--- tests/helpers/server.ts
+++ tests/helpers/server.ts
@@ -1,12 +1,13 @@
 import { Server } from 'http'
 
 import { server as applicationServer } from '~/server'
+import { logger } from '~/common/logger'
 
 export let server: Server
 
 export const startServer = async (): Promise<void> => {
   const app = await applicationServer()
-  server = app.listen(3000, () => console.log('Test server is running on port 3000'))
+  server = app.listen(3000, () => logger.info('Test server is running on port 3000'))
 }
 
 export const closeServer = async (): Promise<void> => {
